83ea40f6e759ae223d7602ed9dea3f53be2a4ace
Rodrigo Alvarenga <00017316@uca.edu.sv>
2018-04-29 20:31:58 -0500

    Added favorite and contacts tab. Also added search bar on toolbar

--- app/src/main/java/com/alvarenga/agenda/MainActivity.kt
+++ app/src/main/java/com/alvarenga/agenda/MainActivity.kt
@@ -2,11 +2,15 @@ package com.alvarenga.agenda
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myToolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
+
     }
 }
